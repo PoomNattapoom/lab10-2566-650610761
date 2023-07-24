55b173f5dfce7a2a99180e93d1655cedb4873f3d
PoomNattapoom <rockyaggi@gmail.com>
2023-07-24 16:12:33 +0700

    ff

--- src/app/page.js
+++ src/app/page.js
@@ -63,10 +63,11 @@ export default function RandomUserPage() {
         !isLoading &&
         users.map((user) => (
           <UserCard
+            key={user.email}
             name={user.name}
             imgUrl={user.imgUrl}
             address={user.address}
-            key={user.email}
+            email={user.email}
           />
         ))}
     </div>

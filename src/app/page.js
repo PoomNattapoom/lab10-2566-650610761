570e9ee584c41605307f60a0c0dd5ca70b0960dd
PoomNattapoom <rockyaggi@gmail.com>
2023-07-24 15:44:35 +0700

    finish

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,20 @@
 "use client";
 
+import { UserCard } from "@/components/UserCard";
+import { cleanUser } from "@/libs/cleanUser";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function RandomUserPage() {
   //user = null or array of object
   const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [genAmount, setGenAmount] = useState(1);
+
+  useEffect(() => {
+    const strUser = JSON.stringify(users);
+    localStorage.setItem("users", strUser);
+  }, [users]);
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
@@ -17,6 +24,8 @@ export default function RandomUserPage() {
     setIsLoading(false);
     const users = resp.data.results;
     //Your code here
+    const cleanedUsers = users.map((user) => cleanUser(user));
+    setUsers(cleanedUsers);
     //Process result from api response with map function. Tips use function from /src/libs/cleanUser
     //Then update state with function : setUsers(...)
   };
@@ -40,7 +49,16 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users &&
+        !isLoading &&
+        users.map((user) => (
+          <UserCard
+            name={user.name}
+            imgUrl={user.imgUrl}
+            address={user.address}
+            email={user.email}
+          />
+        ))}
     </div>
   );
 }
